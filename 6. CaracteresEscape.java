2d3affec73ffb0ed4d57e0b75034f3a48d7c07d9
Gonzalo <mgs.argentum@gmail.com>
2025-09-14 15:39:14 -0300

    Delete 6. CaracteresEscape.java

--- 6. CaracteresEscape.java
+++
@@ -1,8 +0,0 @@
-
-import java.util.Scanner;
-
-public class CaracteresEscape {
-    public static void main(String[] args){
-        System.out.println("Nombre: Juan Pérez\nEdad: 30 años\nDirección: \"Calle Falsa 123\"");
-    }
-}
